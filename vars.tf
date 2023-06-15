665821d7f3c1bc15665e59b19d194f1ea8e0441d
Yngve Bakken-Nilsen <yngve.nilsen@gmail.com>
2023-06-15 11:41:51 +0200

    cleanup: remove service_principal_client_secret

--- vars.tf
+++ vars.tf
@@ -12,12 +12,6 @@ variable "location" {
     default = "Norway East"
 }
 
-output "service_principal_client_secret" {
-  description = "Obfuscate this password with Rikstoto.Obfuscator, and add as client secret in the application configuration in Octopus deploy"
-  value = azuread_application_password.app_password.value
-  sensitive = true
-}
-
 output "service_principal_client_id" {
   description = "value"
   value = azuread_application.azuread_application.application_id
