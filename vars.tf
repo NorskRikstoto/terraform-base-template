6a5e0af1b8375415949463774e756915586fd3b5
Arve Systad <arve.systad@rikstoto.no>
2023-06-20 15:23:44 +0200

    chore: Add back secret output, it's used by dom setupazure

--- vars.tf
+++ vars.tf
@@ -12,7 +12,13 @@ variable "location" {
     default = "Norway East"
 }
 
+output "service_principal_client_secret" {
+  description = "Obfuscate this password with Rikstoto.Obfuscator, and add as client secret in the application configuration in Octopus deploy. This is used by dom setupazure"
+  value = azuread_application_password.app_password.value
+  sensitive = true
+}
+
 output "service_principal_client_id" {
-  description = "value"
+  description = "This is used by dom setupazure"
   value = azuread_application.azuread_application.application_id
 }
