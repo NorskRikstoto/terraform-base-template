346c82b5c22f1b6e15ecaad72a3050d109b66bbf
Arve Systad <arve.systad@rikstoto.no>
2023-11-17 10:55:26 +0100

    chore: ok, ok, re-add this. Twas necessary after all

--- vars.tf
+++ vars.tf
@@ -11,3 +11,14 @@ variable "location" {
     type = string
     default = "Norway East"
 }
+
+output "service_principal_client_secret" {
+  description = "Obfuscate this password with Rikstoto.Obfuscator, and add as client secret in the application configuration in Octopus deploy. This is used by dom setupazure"
+  value = azuread_application_password.app_password.value
+  sensitive = true
+}
+
+output "service_principal_client_id" {
+  description = "This is used by dom setupazure"
+  value = azuread_application.azuread_application.application_id
+}
